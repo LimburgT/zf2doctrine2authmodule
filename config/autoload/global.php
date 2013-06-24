bde0a1360fce095e5e34e0d8ceba9b0e3ae9c417
tobster67 <tobias.limburg@gmx.de>
2013-06-24 09:51:51 +0200

    set to common values

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=zenddoctrineauthentification;host=localhost',
+        'dsn'            => 'mysql:dbname=dbname;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
